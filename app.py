c8b73ad4357cd157db3561a3ca32c9dc730d72b0
Astrophil <luo19902567292@163.com>
2022-04-16 16:40:03 +0800

    更改app

--- app.py
+++ app.py
@@ -1,40 +1,53 @@
-from PySide2.QtWidgets import QApplication, QMessageBox
+from PySide2.QtWidgets import QApplication, QMessageBox, QTableWidgetItem, QGraphicsPixmapItem, QGraphicsScene
 from PySide2.QtUiTools import QUiLoader
+import argparse
+from kitti_odometry import KittiEvalOdom
+import cv2
+from PyQt5.QtGui import QImage, QPixmap
 
 class Stats:
 
     def __init__(self):
-        # 从文件中加载UI定义
-
-        # 从 UI 定义中动态 创建一个相应的窗口对象
-        # 注意：里面的控件对象也成为窗口对象的属性了
-        # 比如 self.ui.button , self.ui.textEdit
         self.ui = QUiLoader().load('ui/ui.ui')
 
-        self.ui.button.clicked.connect(self.handleCalc)
+        self.ui.button.clicked.connect(self.generate)
 
-    def handleCalc(self):
-        info = self.ui.textEdit.toPlainText()
+        self.eval_tool = KittiEvalOdom()
 
-        salary_above_20k = ''
-        salary_below_20k = ''
-        for line in info.splitlines():
-            if not line.strip():
-                continue
-            parts = line.split(' ')
 
-            parts = [p for p in parts if p]
-            name,salary,age = parts
-            if int(salary) >= 20000:
-                salary_above_20k += name + '\n'
-            else:
-                salary_below_20k += name + '\n'
+    def generate(self):
+        # 获取测试序列
+        self.seq = self.ui.comboBox_seq.currentText()
+        self.align = self.ui.comboBox_align.currentText()
 
-        QMessageBox.about(self.ui,
-                    '统计结果',
-                    f'''薪资20000 以上的有：\n{salary_above_20k}
-                    \n薪资20000 以下的有：\n{salary_below_20k}'''
-                    )
+        # 获取测试序列的file_path和gt_path
+        self.file_path = self.ui.lineEdit_test.text()
+        self.gt_path = self.ui.lineEdit_gt.text()
+
+        # 如果没有，则是默认
+        if self.file_path == '':
+            self.file_path = 'test/'
+        if self.gt_path == '':
+            self.gt_path = 'dataset/kitti_odom/'
+
+        # 获取”是否保存结果文件“的勾选信息，勾选了返回True
+        self.if_save = self.ui.radioButton.isChecked()
+
+        self.result = self.eval_tool.eval(
+            self.gt_path,
+            self.file_path,
+            alignment=self.align,
+            seq=self.seq,
+            if_save=self.if_save,
+        )
+
+        # 在列表中打印
+        for i in range(len(self.result)):
+            self.ui.tableWidget.setItem(
+                i-1,
+                1,
+                QTableWidgetItem("{0:.3f}".format(self.result[i]))
+            )
 
 app = QApplication([])
 stats = Stats()
